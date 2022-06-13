a61992d93fb8053d8f0cb4e53d661c5a3d1991f6
Dani <danisuesmith@gmail.com>
2022-06-13 10:38:44 -0700

    commit tree bfcf91e3ab2e8da8565f186bdfb7652c6f62219b

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+  let l = str.length;
+  for (let i = 0; i < l / 2; i++) {
+    if (str[i] !== str[l - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  function takes a word as the argument and returns true 
+  if the word is a palindrome 
 */
 
 /*
-  Add written explanation of your solution here
+ 1. First need to consider any non one word strings. => can't get to work
+ 2.  .reverse() only works with arrays so need to turn my string into an array
+ 3. reverse the array
+ 4. put it back together
+ 5. compare the intial string to the reversed one
 */
 
 // You can run `node index.js` to view these console logs
